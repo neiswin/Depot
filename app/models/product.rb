1d5d60ce20ef0f1d969767a003725d2feb8816ea
neiswin <nikolaswind@gmail.com>
2021-12-09 16:23:18 +0300

    indexDone

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
 validates :title, :description, :image_url, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
+validates :title, length: { maximum: 15 }
 validates :image_url, allow_blank: true, format: {
 	with: %r{\.(gif|png|jpg)\Z}i,
 	message: 'URL должен быть в формате jpg, png или gif'
